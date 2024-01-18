9a6a9bc13daa7cd91e150d602e8bfd772a88e442
wenchicheng <wenchicheng@outlook.com>
2024-01-18 23:55:26 +0800

    change color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,13 +14,16 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'light',
     themes: {
-      dark: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6'
-        }
+      light: {
+        primary: '#E57373',
+        secondary: '#424242',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#CCFF90',
+        warning: '#FDD835',
+        background: '#FFEBEE'
       }
     }
   }
